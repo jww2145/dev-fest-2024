0161f555d97408583dcc70b40d495161b8341b66
jww2145 <jww2145@columbia.edu>
2024-02-03 17:55:49 -0500

    Adding frontend

---
+++ myapp/src/components/Senate-Child.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Senators from "./Senators.svelte";
+
+</script>
+
+<div class = 'container'>
+    <div class = 'container-child-left'>Test Test</div>
+    <div class = 'container-child-right'><Senators /></div>
+</div>
+
+<style>
+
+.container{
+        display:flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-around;
+        align-items: center;
+        align-content: center;
+    }
+
+    .container-child-left{
+        width: 27%;
+        padding:3%;
+        background-color: rgb(216, 241, 255,.5);
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-size:12pt;
+        text-align:center;
+        border: 1px solid #005682;
+    }
+
+    .container-child-right{
+        width: 60%;
+        padding: 1%;
+        background-color:rgb(216, 241, 255,.5);
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-size: 12pt;
+        text-align: center;
+        border: 1px solid #005682;
+    }
+</style>
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Header from '../components/Header.svelte'
+    import SenateChild from '../components/Senate-Child.svelte';
+</script>
+
+<Header />
+<SenateChild/>
