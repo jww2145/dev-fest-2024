cd9c4d4e48348995000ee1f0ca60dcbe35adf111
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2024-02-04 04:00:50 -0500

    bruhh

--- myapp/src/components/senate-charts/line-chart.svelte
+++ myapp/src/components/senate-charts/line-chart.svelte
@@ -2,11 +2,8 @@
   import { onMount } from "svelte";
   import * as d3 from "d3";
 
-<<<<<<< HEAD
   export const prerender = true;
 
-=======
->>>>>>> cf2458e (added a dropdown for relevance graph)
   let svg;
   let issues;
   let x;
@@ -14,7 +11,6 @@
   let myLine;
 
   onMount(() => {
-<<<<<<< HEAD
     const margin = { top: 60, right: 80, bottom: 30, left: 50 };
     const width = 1000 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
@@ -33,23 +29,10 @@
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-=======
-    const margin = { top: 20, right: 80, bottom: 30, left: 50 };
-    const width = 1000 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-
-    svg = d3.select("svg");
-
-    const g = svg
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-
->>>>>>> cf2458e (added a dropdown for relevance graph)
     x = d3.scaleLinear().domain([1971, 2023]).range([0, width]);
     y = d3.scaleLinear().domain([1, 0]).range([height, 0]);
 
     myLine = g.append("path");
-<<<<<<< HEAD
 
 
     const line = d3
@@ -83,49 +66,6 @@
           .attr("class", "axis axis--x")
           .attr("transform", `translate(0,${height})`)
           .call(d3.axisBottom(x).tickFormat(d3.format("d")));
-=======
-
-    const clip = svg
-      .append("svg:clipPath")
-      .attr("id", "clip")
-      .append("svg:rect")
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("width", width)
-      .attr("height", height);
-
-    const line = d3
-      .line()
-      .curve(d3.curveBasis)
-      .x((d) => x(d.time))
-      .y((d) => y(d.rate));
-
-    d3.csv("https://gist.githubusercontent.com/jww2145/412d865cd12317e9e8486194cd23c229/raw/386bf6fcc73542d9883159e0fb52264cc5139eec/senate-issues.csv").then(function(data){
-      issues = data.columns.slice(1).map((id) => {
-          return {
-            id: id,
-            values: data.map((d) => {
-              return {
-                time: +d.time,  // Convert to number
-                rate: +d[id],   // Convert to number
-              };
-            }),
-          };
-        });
-
-
-        x.domain(d3.extent(data, (d) => d.time));
-
-        y.domain([
-          d3.min(issues, (c) => d3.min(c.values, (d) => d.rate)),
-          d3.max(issues, (c) => d3.max(c.values, (d) => d.rate)),
-        ]);
-
-        g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", `translate(0,${height})`)
-          .call(d3.axisBottom(x));
->>>>>>> cf2458e (added a dropdown for relevance graph)
 
         g.append("g")
           .attr("class", "axis axis--y")
@@ -135,7 +75,6 @@
           .attr("y", 6)
           .attr("dy", "0.71em")
           .attr("fill", "#000")
-<<<<<<< HEAD
           .text("Percent");
 
         // Horizontal grid lines
@@ -154,9 +93,6 @@
           .call(d3.axisBottom(x).tickSize(-height).tickFormat(''));
 
 
-=======
-          .text("Count");
->>>>>>> cf2458e (added a dropdown for relevance graph)
 
         const selector = d3.select("#issue_list").append("select");
 
@@ -187,7 +123,6 @@
 </script>
 
 <style>
-<<<<<<< HEAD
   .parent{
     display: flex;
     flex-direction: row;
@@ -211,24 +146,6 @@
    </div>
  </div>
 
-=======
-  .axis--x path {
-    display: none;
-  }
-
-  .line {
-    fill: none;
-    stroke: steelblue;
-    stroke-width: 1.5px;
-  }
-</style>
-
-<body>
-  <svg width="1000" height="500"></svg>
-  <div id="issue_list"></div>
->>>>>>> cf2458e (added a dropdown for relevance graph)
 </body>
 
 <!--Brian was Here-->
-
-
