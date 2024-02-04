448cff9f01a0b3b32b4417508d348f246b108ae8
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2024-02-03 20:49:54 -0500

    yes

--- data-exploration/data-exploration.py
+++ data-exploration/data-exploration.py
@@ -88,7 +88,7 @@ for y in years:
 
 issues_by_year =  issues_by_year[::-1] #sorting years ascending instead of descending
 #used one to create a json for webapp
-issues_by_year.to_json('issues.json', orient='records', lines=True)
+#issues_by_year.to_json('issues.json', orient='records', lines=True)
 
 #plot the issues relevance over time
 for column in issues_by_year.columns:
