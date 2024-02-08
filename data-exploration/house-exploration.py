d9dbb7065645407bc4e31ef6707adccf2e5b89d0
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2024-02-07 22:52:19 -0500

    updated issues.csv

--- data-exploration/house-exploration.py
+++ data-exploration/house-exploration.py
@@ -11,6 +11,9 @@ houseDF["Vote_Year"] = houseDF["Vote_Year"].astype(int)
 #creates a list of the current representatives
 houseList = houseDF.head(435).Representative.values
 
+#create a list of the years in our data set
+years = houseDF.Vote_Year.unique()
+
 #function to split the issues into their individual components
 #only works with 2022 data and further in the past
 def splitter(row):
@@ -62,5 +65,19 @@ issuesDF = houseDF.explode("Vote_Issues")
 
 #find all the issues that are discussed
 issue_list = list(set(issuesDF["Vote_Issues"].tolist()))
+issues_by_year = pd.DataFrame(0.0, index = years, columns = issue_list)
 
-print(houseList)
+#create a dictionary for the relevance of an issue in a particular year
+for y in years:
+
+    info = houseDF[houseDF["Vote_Year"]==y]
+    num_bills = info.shape[0]
+    counted_values = Counter(issue for issues in info["Vote_Issues"] for issue in issues)
+    for item, amt in counted_values.items():
+        issues_by_year.loc[y, item] = amt/num_bills
+
+issues_by_year =  issues_by_year[::-1] #sorting years ascending instead of descending
+
+#used once to create a csv for webapp
+issues_by_year.to_csv('issues.csv', index_label="time")
+
