5e56d33afb2c73837157211cd280984327421b49
jww2145 <jww2145@columbia.edu>
2024-02-18 12:34:55 -0500

    made clusters

--- data-exploration/cluster-maker.py
+++ data-exploration/cluster-maker.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 from senators import senators
+from sklearn.preprocessing import StandardScaler
+import json
+
 
 def kmeans_senate_votes(csv_file):
 
@@ -22,14 +25,28 @@ def kmeans_senate_votes(csv_file):
     # Filter out '?' votes
     df = df[df['Vote'] != '?']
 
-    # Encode votes
-    df = df.replace(['+'], [1])
-    df = df.replace(['-'], [-1]) 
+    senator_ids = df['Senator'].unique()
 
     senator_votes = df.groupby('Senator').agg({'Vote': lambda x: (x=='+').sum()/len(x)})
-
-    # Cluster
+    
     kmeans = KMeans(n_clusters=4).fit(senator_votes)
-    labels = kmeans.labels_
+    
+    clusters = []
+    for senator_id in senator_ids:
+        name = le.inverse_transform([senator_id])[0] 
+        cluster = kmeans.labels_[senator_id]
+        percentage = senator_votes.loc[senator_id, 'Vote']
+        clusters.append((name, str(cluster), round(percentage, 2)))
 
-    return labels
+    return clusters 
+
+
+
+
+json_data = kmeans_senate_votes('/Users/joshiebestie/Coding Projects/dev-fest-2024/csv-library/senate_votes copie.csv')
+
+print(kmeans_senate_votes('/Users/joshiebestie/Coding Projects/dev-fest-2024/csv-library/senate_votes copie.csv'))
+
+
+with open('clusters.json', 'w') as f:
+  json.dump(json_data, f)
